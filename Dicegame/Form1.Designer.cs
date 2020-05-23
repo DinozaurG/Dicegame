ac8c3cfea7b3075785de571e6e6afb71c8030acb
Dinozaur_G <karnah@mail.ru>
2020-05-23 16:36:40 +0700

    asdf

--- Dicegame/Form1.Designer.cs
+++ Dicegame/Form1.Designer.cs
@@ -28,20 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonForDicing = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelForResult = new System.Windows.Forms.Label();
+            this.buttonForCheating = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonForDicing
+            // 
+            this.buttonForDicing.Location = new System.Drawing.Point(68, 86);
+            this.buttonForDicing.Name = "buttonForDicing";
+            this.buttonForDicing.Size = new System.Drawing.Size(193, 118);
+            this.buttonForDicing.TabIndex = 0;
+            this.buttonForDicing.Text = "Бросить кубики!";
+            this.buttonForDicing.UseVisualStyleBackColor = true;
+            this.buttonForDicing.Click += new System.EventHandler(this.buttonForDicing_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 74);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Игра, в которой три шестигранных кубика. Бросайте кубики по очереди. У кого больш" +
+    "е сумма очков, тот и победит!";
+            // 
+            // labelForResult
+            // 
+            this.labelForResult.AutoSize = true;
+            this.labelForResult.Location = new System.Drawing.Point(52, 242);
+            this.labelForResult.Name = "labelForResult";
+            this.labelForResult.Size = new System.Drawing.Size(0, 17);
+            this.labelForResult.TabIndex = 2;
+            // 
+            // buttonForCheating
+            // 
+            this.buttonForCheating.Location = new System.Drawing.Point(316, 298);
+            this.buttonForCheating.Name = "buttonForCheating";
+            this.buttonForCheating.Size = new System.Drawing.Size(17, 17);
+            this.buttonForCheating.TabIndex = 3;
+            this.buttonForCheating.UseVisualStyleBackColor = true;
+            this.buttonForCheating.Click += new System.EventHandler(this.buttonForCheating_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(328, 310);
+            this.Controls.Add(this.buttonForCheating);
+            this.Controls.Add(this.labelForResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonForDicing);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonForDicing;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelForResult;
+        private System.Windows.Forms.Button buttonForCheating;
     }
 }
 
--- Dicegame/Form1.cs
+++ Dicegame/Form1.cs
@@ -12,9 +12,49 @@ namespace Dicegame
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
+        double[] possibilites = new double[6] {0.166666667, 0.166666667, 0.166666667, 0.166666667, 0.166666667, 0.166666667};
+        int[] dice = new int[3];
+        Boolean CheatsOn = false;
         public Form1()
         {
             InitializeComponent();
+        }
+        private void buttonForDicing_Click(object sender, EventArgs e)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                double a = rnd.NextDouble() % 1;
+                for (int i = 1; i <= 6; i++)
+                {
+                    a -= possibilites[i - 1];
+                    if (a <= 0)
+                    {
+                        dice[j] = i;
+                        break;
+                    }
+                }
+            }
+            labelForResult.Text = "Вам выпадает " + dice[0] + "," + dice[1] + "," + dice[2] + ". " + "Их сумма:" + (dice[0] + dice[1] + dice[2]);
+            if (CheatsOn)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    possibilites[j] = 0.166666667;
+                }
+                CheatsOn = false;
+            }
+        }
+
+        private void buttonForCheating_Click(object sender, EventArgs e)
+        {
+            CheatsOn = true;
+            possibilites[0] = 0;
+            possibilites[1] = 0;
+            possibilites[2] = 0;
+            possibilites[3] = 0.05;
+            possibilites[4] = 0.25;
+            possibilites[5] = 0.7;
         }
     }
 }
